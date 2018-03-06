5197b9dcb4dd2c10b6e7e37424cec16a3168f486
Loïc Brisset <loic@square1.io>
2018-03-06 16:17:28 +0000

    Rename Client to SDK

---
+++ src/RiotSDK.php
@@ -0,0 +1,70 @@
+<?php
+namespace Lbrs\Riot;
+use GuzzleHttp\Client as Http;
+
+/**
+ * Riot API implementation.
+ *
+ * @version 1.0.0
+ * @author  Loïc Brisset
+ */
+class RiotSDK
+{
+    const API_BASE_URI = 'https://euw1.api.riotgames.com';
+
+    /**
+     * @var Http
+     */
+    private $http;
+
+    /**
+     * @var string
+     */
+    private $key;
+
+    /**
+     * Create a new client instance.
+     *
+     * @param string $apiKey.
+     */
+    public function __construct($apiKey)
+    {
+        $this->http = new Http([
+            'base_uri' => self::API_BASE_URI,
+            'headers' => [
+                'Accept' => 'application/json',
+            ]
+        ]);
+
+        $this->key = $apiKey;
+    }
+
+    /**
+     * Get a Summoner from his name.
+     *
+     * @param $name
+     * @return array|object
+     */
+    public function getSummoner($name)
+    {
+        return $this->call('GET', '/lol/summoner/v3/summoners/by-name/' . $name);
+    }
+
+    /**
+     * Send a request to the API.
+     *
+     * @param  string $method The HTTP method.
+     * @param  string $endpoint The endpoint.
+     * @param  array $params The params to send with the request.
+     * @return array|object The response as JSON.
+     */
+    private function call($method, $endpoint, array $params = null)
+    {
+        $options = [];
+        if (!empty($params)) {
+            $options['json'] = $params;
+        }
+        $response = $this->http->request($method, $endpoint, $options);
+        return json_decode($response->getBody());
+    }
+}
